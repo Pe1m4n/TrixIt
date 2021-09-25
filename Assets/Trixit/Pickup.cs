6978113e1bf01950ecb786b912bfa585ff89d32e
g.khokhlov <g.khokhlov@corp.nekki.ru>
2021-09-26 01:32:12 +0300

    Pickup component

---
+++ Assets/Trixit/Pickup.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+namespace Trixit
+{
+    [RequireComponent(typeof(Collider))]
+    public class Pickup : MonoBehaviour
+    {
+        public bool Finish;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.layer != LayerMask.NameToLayer("Character"))
+            {
+                return;
+            }
+
+            if (Finish)
+            {
+                GlobalController.PlayLevel(++GlobalController.CurrentLevel);
+            }
+            else
+            {
+                GlobalController.CurrentLevelScore++;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
