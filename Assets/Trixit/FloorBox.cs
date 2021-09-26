28d71345ba8e217dea33a8200b84dba5c214c3cc
g.khokhlov <g.khokhlov@corp.nekki.ru>
2021-09-26 15:35:50 +0300

    12.4 first jump

--- Assets/Trixit/FloorBox.cs
+++ Assets/Trixit/FloorBox.cs
@@ -9,13 +9,14 @@ namespace Trixit
         public BoxType BoxType;
         public bool Immutable;
         public float AnglesFromForward = 30f;
-        public float JumpForce = 11f;
+        public float JumpForce = 12.4f;
         public float JumpSecond = 11f;
         public ForceMode ForceMode = ForceMode.Acceleration;
         public float DestroyTime = 2f;
         [SerializeField] private AudioClip _bounceSound;
         [SerializeField] private AudioClip _landingHeavy;
         [SerializeField] private AudioClip _landing;
+        [SerializeField] private AudioClip _finishSound;
 
         private bool _scheduledDestroy;
         private float _timeUntilDestroy;
